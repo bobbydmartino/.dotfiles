a60e1f32d178b04e87b8acd8589546486e2dd630
Bobby Martino <bobbydmartino@gmail.com>
2023-01-02 14:47:12 +0000

    updates for new system

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 
+#TODO: track what already exists and save to backup.yaml
 
 if [ ! -f "/usr/share/fonts/JetBrains Mono Bold Nerd Font Complete.ttf" ]
 then
@@ -9,6 +10,42 @@ then
 	curl -fLo "/usr/share/fonts/JetBrains Mono Regular Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/JetBrainsMono/Ligatures/Regular/complete/JetBrains%20Mono%20Regular%20Nerd%20Font%20Complete.ttf
 fi
 
+sudo apt-get update && sudo apt-get install -y \
+	tmux \
+	wget \
+	zsh \
+	sxiv \
+	mediainfo \ 
+	bat \
+	curl \
+	rsync \
+	git \
+	zathura \
+	mpv \
+	python3-pip \
+	trash-cli \
+	neofetch \
+	graphicsmagick \
+	ffmpegthumbnailer 
+
+
+# mv .config and .local to .old/.config and .old/.local along with .zprofile, .zshrc, .tmux.conf, .tmux
+
+mkdir -p ~/.cache
+mkdir -p ~/.cache/zsh
+touch ~/.cache/history
+
+ln -sf ~/.config/shell/profile ~/.zprofile
+ln -sf ~/.config/zsh/.zshrc ~/.zshrc
+ln -sf ~/.config/tmux/.tmux.conf ~/.tmux.conf
+mkdir -p ~/.tmux/
+export PATH=$PATH:~/.local/bin/ 
+ln -sf ~/.config/tmux/plugins/ ~/.tmux/plugins
+ln -sf ~/.config/tmux/resurrect ~/.tmux/resurrect
+
+#check if .vscode-server is installled
+  #if not install vscode server somehow
+  #if so then install extensions for programming
 
 #INSTALL PLUGINS FROM SOURCE
 while IFS="" read -r p || [ -n "$p" ]
@@ -23,11 +60,7 @@ do
   then
           git clone ${arrIN[2]} ".config/${arrIN[0]}/plugins/${arrIN[1]}"
   fi
-done < .pluginlist
+done < .config/plugins/.pluginlist
 
 
-cp ~/.Xauthority .
-
-docker-compose up -d --build
-docker exec -it dotfiles_dot_1 /root/.config/setup.sh
-
+chsh -s /usr/bin/zsh
---
+++ uninstall.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#remove fonts files
+
+#apt remove sxiv, mediainfo, bat, zathura, mpv, trash-cli, neofetch, graphicsmagick, ffmpegthumbnailer
+
+#TODO: if something already installed, keep it, else remove
+#TODO: track prevshell and change back to that
+
+#if .old exists, else throw error and do not remove
+    #TODO: prompt if want to go back to ubuntu basic configuration, and copy those files in
+
+#rm .config and .local and .zprofile etc
+#mv .old/* . and rmdir .old
+
+#chsh -s prevshell
